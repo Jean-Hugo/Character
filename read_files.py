1e9da13d57f16af77b5d96c1fec3295614d712a9
Jackeastern <oostenjac@gmail.com>
2022-09-03 23:32:20 +0200

    additional_test

---
+++ read_files.py
@@ -0,0 +1,14 @@
+"""This code file opens files and prints their content"""
+def read_file(filename:str):
+    file_content = None
+    # TODO Open file and read contents then
+    return file_content
+
+def print_file_contnet(content):
+    """Prints the file contet"""
+    # TODO print content
+    print(content)
+
+def main():
+    read_file
+    print_file_contnet
