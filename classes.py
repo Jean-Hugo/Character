764c6d4f84ebfc7a7a79f5d360ff49926b6139a6
Jean-Hugo <truterjeanhugo@gmail.com>
2022-09-03 23:29:26 -0700

    Egg

--- classes.py
+++ classes.py
@@ -5,13 +5,15 @@ class Person:
         self.height = height
         self.first_name = first_name
         self.last_name = last_name
+        print("my naam is " + user.first_name + " " + user.last_name)
+        print("My gewig is " + str(user.weight))
+        print("My lengte is " + str(user.height))
 
-user = Person()
-
+user = Person() # default python constructer word hier gebruik
 user.age = 25
 user.weight = 80
 user.height = 177
 user.first_name = "Persoon"
 user.last_name = "White"
 
-print(user.weight)
+user.self()
