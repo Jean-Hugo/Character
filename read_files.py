7627bc7b89bfa91ad913a0dec4ddc696ed24e1ab
Jean-Hugo <truterjeanhugo@gmail.com>
2022-09-04 01:20:25 -0700

    Egg

--- read_files.py
+++ read_files.py
@@ -1,14 +1,19 @@
 """This code file opens files and prints their content"""
 def read_file(filename:str):
-    file_content = None
     # TODO Open file and read contents then
+    file_content = []
+    file = open(filename, "r+")
+    for line in file.readlines():
+        file_content.append(line)
     return file_content
 
 def print_file_contnet(content):
-    """Prints the file contet"""
+    """Prints the file content"""
     # TODO print content
     print(content)
 
 def main():
-    read_file
+    read_file 
     print_file_contnet
+
+challenge = read_file("challenge.txt")
