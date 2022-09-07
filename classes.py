aa22f030f9cc976f9e28983332e18cc0c06d4fc3
Jean-Hugo <truterjeanhugo@gmail.com>
2022-09-06 18:24:41 -0700

    Egg

--- classes.py
+++ classes.py
@@ -1,19 +1,12 @@
 class Person:
-    def self(self, age, weight, height, first_name, last_name):
+    # self verwys na hele ding en dit moet altyd eerste parameter wees
+    def __init__(self, age, weight, height, first_name, last_name):
         self.age = age
         self.weight = weight
         self.height = height
         self.first_name = first_name
         self.last_name = last_name
-        print("my naam is " + user.first_name + " " + user.last_name)
-        print("My gewig is " + str(user.weight))
-        print("My lengte is " + str(user.height))
 
-user = Person() # default python constructer word hier gebruik
-user.age = 25
-user.weight = 80
-user.height = 177
-user.first_name = "Persoon"
-user.last_name = "White"
+user = Person(25, 80, 177, "Persoon", "White") # default python constructer word hier gebruik
 
-user.self()
+print(user.last_name)
