8750932aa5672c553874be126018ecc0144b7187
Jean-Hugo <truterjeanhugo@gmail.com>
2022-09-06 19:23:20 -0700

    Egg

---
+++ SQLite.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+# define connection and cursor
+# connection: represents the database
+# cursor: used to interact with the database
+
+connection = sqlite3.connect('store_transactions.db')
+
+cursor = connection.cursor()
+
+# create stores table
+
+command1 = """CREATE TABLE IF NOT EXISTS
+stores(store_id INTEGER PRIMARY KEY, location TEXT)"""
+
+cursor.execute(command1)
+
+# create purchases table
+
+command2 = """CREATE TABLE IF NOT EXISTS
+purchases(purchase_id INTEGER PRIMARY KEY, store_id INTEGER, total_cost FLOAT, FOREIGN KEY(store_id) REFERENCES stores(store_id))"""
