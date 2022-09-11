57cbd12d72620d64c2d19afb67c19368a102fad2
Jean-Hugo <truterjeanhugo@gmail.com>
2022-09-10 21:32:22 -0700

    Egg

---
+++ database/app_database_enginr.py
@@ -0,0 +1,26 @@
+"""Has the code that drives the database"""
+import database.app_datamodel as database_model
+import database.app_database as database_connect
+
+def create_record(table, record):
+    """Creates record in table"""
+    return
+
+def load_record(table, term):
+    """Searches the table and term"""
+    return
+
+def insert_attendence(timestamp, person_id):
+    """Creates the record of the attendence"""
+    return
+
+def get_person(id, name, surname):
+    """Gets the person's information"""
+    return
+
+def get_account(account_number, account_balance):
+    """Gets the person's account details"""
+    return
+
+def insert_person():
+    return
--- database/app_datamodel.py
+++ database/app_datamodel.py
@@ -1,2 +1,21 @@
 """OK this is a code file that helps us create the database model"""
 from dataclasses import dataclass
+
+
+class Person:
+    """It stores the information of n person"""
+    id: int
+    name: str
+    surname: str
+
+
+class Account:
+    """It stores the information of their account"""
+    account_number: int
+    account_balance: int
+
+
+class Attendence:
+    """Store when they sign in for work"""
+    timestamp: int
+    id: int
