c11524bdbd86d6a439c77225db75013ebf3eec82
Jackeastern <oostenjac@gmail.com>
2022-09-17 22:32:12 +0200

    unitTests

---
+++ unit_tests_main.py
@@ -0,0 +1,26 @@
+import unittest
+
+
+def exponent(x_values):
+    """Returns the exponent of the value supplied"""
+    return x_values * x_values
+
+
+class TestMathFunction(unittest.TestCase):
+    """Basic Example"""
+    def test_Exponent(self):
+        """Tests if the exponent is given"""
+        self.assertEqual(exponent(2), 4)
+    
+    def test_Fail(self):
+        """Failed Example"""
+        self.assertEqual(exponent(3), 9)
+
+
+
+class TestUserInputValidations(unittest.TestCase):
+    """Test the validation checks of the user interface"""
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
